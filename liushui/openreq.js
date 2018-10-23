a3a8a54bb7d3222dc56ff915af2ca5fd2878202f
yhuisy <laiyuehui@shangyoo.com>
2018-10-23 14:19:27 +0800

    'reset'

--- liushui/openreq.js
+++ liushui/openreq.js
@@ -1,3 +1,5 @@
+
+
 #!/root/code/tools/node/node4.x64
 
 var http = require('http');
@@ -8,11 +10,13 @@ var request = require('request');
 const cluster = require('cluster');
 
 
-      var count = 10000
-    var pos = 0
+var count = 1000
+var pos = 0
+var random = Math.ceil(Math.random()*100000)
 
-var pw= 0
-
+var pw = 0
+var url = 'http://party3hxddz.syyx.com/mj_game_party/init?&sid=1&chn=20&ver=100&uid='+Math.ceil(Math.random()*1000000)+'&award_type='+Math.ceil(Math.random()*2)+'&award_rank='+Math.ceil(Math.random()*4)
+url = 'http://pf.nycs.syyx.cn/army_has_attended?account=liushui&server_id='
 
 var run_worker = function () {
 
@@ -24,26 +28,30 @@ return
 var main = function(){
 pw++
       request({
-            url: 'http://192.168.1.60:8000?request='+cluster.worker.id+'&pw='+pw,
+            url: url+Math.ceil(Math.random()*20)+'&cluster='+cluster.worker.id,
+            // url: 'http://192.168.1.60:8000?request='+cluster.worker.id+'&pw='+pw,
             method: "GET"
         }, function(err, req, body) {
             if(pos<count){
         pos++;
-	main();
-	console.log(body)
-	
-        var data = body.toString().split(/[?&]/)
-        var arg = {}
-        data.forEach(function (item) {
-            arg[item.split('=')[0]] = item.split('=')[1];
-        })
+    main();
+    // console.log(body)
+    var data=JSON.parse(body)
+    console.log(pos,'-----',data)
 
-	if(arg.ret=='1'){
-		process.send({ cmd: 'ok' });	
-	}
+	
+    //     var data = body.toString().split(/[?&]/)
+    //     var arg = {}
+    //     data.forEach(function (item) {
+    //         arg[item.split('=')[0]] = item.split('=')[1];
+    //     })
+
+	// if(arg.ret=='1'){
+	// 	process.send({ cmd: 'ok' });	
+	// }
 
         }       
-        });
+         });
 return 
 
 }
@@ -79,3 +87,4 @@ if (cluster.isMaster) {
 
     run_worker();
 }
+
