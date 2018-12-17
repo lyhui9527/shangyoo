a66ee18005b90ab96c0d479860a954c37c8aa95b
yhui <lyhui9527@outlook.com>
2018-12-18 00:24:58 +0800

    'big_filefile_to_mem'

--- liushui/support.js
+++ liushui/support.js
@@ -1,25 +1,7 @@
 var i = 1;
 setInterval(function() {
-	console.log(new Date(), i);
+    console.log(new Date(), i);
 
-	i++;
+    i++;
 
-<<<<<<< HEAD
-
-
-// var i=1;
-//     setInterval(function(){
-//         console.log(new Date(),i );
-
-//         i++;
-
-//     },1000)
-
-
-
-for (var i =1;i<60;i++){
-	console.log(Math.ceil(Math.random() * 1000000))
-}
-=======
 }, 1000)
->>>>>>> db02731cd61de4a2565f96fc8f95573d253624e0
--- liushui/sy_code/hello.js
+++ liushui/sy_code/hello.js
@@ -14,7 +14,22 @@ var date1 = [
 ]
 
 
-var date2 = [1, 2, 3, 4, 2, 11, 22, 21, 25, 31]
+var date2 = [{
+	a: 12,
+	b: '1-1-1'
+}, {
+	a: 13,
+	b: '2-2-2'
+}, {
+	a: 14,
+	b: '3-3-3'
+}, {
+	a: 15,
+	b: '4-4-4'
+}, {
+	a: 16,
+	b: '5-5-5'
+}]
 
 
 
@@ -29,26 +44,44 @@ var sort_desc = function(a, b) {
 
 
 
-console.log(date1.sort(sort_desc))
+var fun1 = function() {
+	console.log(date1.sort(sort_desc))
 
-if(temp.indexOf('temp/nycs_')){
-	ret=temp.spilt('temp')[1]
-	out
-}
-if(temp.indexOf('serverid=')){
-	ret ='serverid'+temp.spilt('serverid')[1]
-	out
-}
-if(temp.indexOf('=')){
-	ret ='serverid'+temp.spilt('serverid')[1]
-	out
-}
-if(temp.indexOf('active=')){
-	ret ='active'+temp.spilt('active')[1]
-	out
-}
-if(temp.indexOf('active=')){
-	ret ='active'+temp.spilt('active')[1]
-	out
+	if (temp.indexOf('temp/nycs_')) {
+		ret = temp.spilt('temp')[1]
+		out
+	}
+	if (temp.indexOf('serverid=')) {
+		ret = 'serverid' + temp.spilt('serverid')[1]
+		out
+	}
+	if (temp.indexOf('=')) {
+		ret = 'serverid' + temp.spilt('serverid')[1]
+		out
+	}
+	if (temp.indexOf('active=')) {
+		ret = 'active' + temp.spilt('active')[1]
+		out
+	}
+	if (temp.indexOf('active=')) {
+		ret = 'active' + temp.spilt('active')[1]
+		out
+	}
+
 }
 
+var fun2 = function() {
+	console.log(date1.indexOf('2018-11'), "===", date1.indexOf('2018-11-26'))
+
+	var re1 = date2.find(function(item) {
+		return (item.a>13)
+	})
+	console.log(re1)
+
+	var re2 = date2.filter(function(item){
+		return item.a<14
+	})
+	console.log(re2)
+
+}
+fun2()
